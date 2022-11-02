d0933557a81949b649392e2e78976e4d76482cbf
Gastón Acosta <gastoneacosta@outlook.com>
2022-11-02 05:24:27 -0300

    a

--- server/core/utils/util.py
+++ server/core/utils/util.py
@@ -12,32 +12,29 @@ def loadDatasets(path):
     return pd.read_csv(path)
 
 
-def splitDatasets(data, n=102, val_percentage=0.1, justTrain=False):
+def splitDatasets(data, n=102, val_percentage=0.1):
 
-    if justTrain == False:
-        test_percentage = 0.1
-        test_limit = int(test_percentage * data[:, 1].size)
-        data_test = data[0:test_limit].T
-        np.random.shuffle(data_test)
-        Y_test = one_hot(data_test[0])
-        X_test = data_test[2:n].T
 
-        validation_limit = int((1 - val_percentage) * data[:, 1].size)
-        data_train = data[test_limit:validation_limit].T
-        np.random.shuffle(data_train)
-        Y_train = one_hot(data_train[0])
-        X_train = data_train[2:n].T
+    np.random.shuffle(data_test)
+    test_percentage = 0.1
+    test_limit = int(test_percentage * data[:, 1].size)
+    data_test = data[0:test_limit].T
+    np.random.shuffle(data_test)
+    Y_test = one_hot(data_test[0])
+    X_test = data_test[2:n].T
 
-        data_val = data[validation_limit:data[:, 1].size].T
-        np.random.shuffle(data_val)
-        Y_val = one_hot(data_val[0])
-        X_val = data_val[2:n].T
+    validation_limit = int((1 - val_percentage) * data[:, 1].size)
+    data_train = data[test_limit:validation_limit].T
+    np.random.shuffle(data_train)
+    Y_train = one_hot(data_train[0])
+    X_train = data_train[2:n].T
 
-    if justTrain:
-        data_train = data[test_limit:validation_limit].T
-        np.random.shuffle(data_train)
-        Y_train = one_hot(data_train[0])
-        X_train = data_train[2:n].T
+    data_val = data[validation_limit:data[:, 1].size].T
+    np.random.shuffle(data_val)
+    Y_val = one_hot(data_val[0])
+    X_val = data_val[2:n].T
+
+
 
     return X_test, Y_test, X_train, Y_train, X_val, Y_val
 
